d067b86a2959d72d6b30fe9450be435f4abcca1b
Chandima Ranaweera <chan4lk@gmail.com>
2023-02-12 09:40:05 +0530

    Initial commit

---
+++ apps/employee-management/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'employee-management',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/employee-management',
+};
