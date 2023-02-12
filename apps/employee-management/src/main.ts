d8c4e2d09173761503931ba84ccd54c790eb8d46
Chandima Ranaweera <chan4lk@gmail.com>
2023-02-12 09:56:58 +0530

    add swagger

--- apps/employee-management/src/main.ts
+++ apps/employee-management/src/main.ts
@@ -7,11 +7,22 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+  .setTitle('Data example')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .addTag('default', 'The Data Endpoints')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
