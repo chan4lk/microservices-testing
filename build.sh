e4e097f173bf87289df6afca2f829a1a8de389a1
Chandima Ranaweera <chan4lk@gmail.com>
2023-02-25 22:57:26 +0530

    k8s working locally

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-nerdctl build -t chan4lk/auth-api . -f apps/auth-api/dockerfile
-nerdctl build -t chan4lk/employee-api . -f apps/employee-management/dockerfile
+nerdctl build -t localhost:5000/auth-api . -f apps/auth-api/dockerfile
+nerdctl build -t localhost:5000/employee-api . -f apps/employee-management/dockerfile
+nerdctl push localhost:5000/auth-api
+nerdctl push localhost:5000/employee-api
