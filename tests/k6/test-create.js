090775ac5a71c15d5b1142a8770a915a8fe35092
Chandima Ranaweera <chan4lk@gmail.com>
2023-02-27 00:14:16 +0530

    clean up

--- tests/k6/test-create.js
+++ tests/k6/test-create.js
@@ -4,6 +4,17 @@ import { Rate } from 'k6/metrics';
 
 const errorRate = new Rate('errorRate');
 
+const payload = JSON.stringify({
+  userName: 'chandima',
+  password: 'super_Scre@t',
+});
+
+const params = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
 export const options = {
   duration: '1m',
   vus: 50,
@@ -15,39 +26,16 @@ export const options = {
     ],
     http_req_duration: ['p(95)<2500'], // 95 percent of response times must be below 500ms
   },
-  
 };
 
 export function setup() {
   const signupUrl = `http://${__ENV.API_HOST}/api/signup`;
-  
-  const payload = JSON.stringify({
-    "userName": "chandima",
-    "password": "super_Scre@t"
-  });
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-
- http.post(signupUrl, payload, params);
+  http.post(signupUrl, payload, params);
 }
 
 export default function () {
   const signInUrl = `http://${__ENV.API_HOST}/api/signin`;
-  
-  const payload = JSON.stringify({
-    "userName": "chandima",
-    "password": "super_Scre@t"
-  });
-
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
 
   const response = http.post(signInUrl, payload, params);
 
