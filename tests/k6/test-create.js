a26d6331e2f97dfc6df25d1f70c898f99374b88c
Chandima Ranaweera <chan4lk@gmail.com>
2023-02-26 23:56:08 +0530

    tweak values

--- tests/k6/test-create.js
+++ tests/k6/test-create.js
@@ -8,10 +8,10 @@ export const options = {
   duration: '1m',
   vus: 50,
   thresholds: {
-    http_req_failed: ['rate<0.05'], // http errors should be less than 1%
+    http_req_failed: ['rate<0.08'], // http errors should be less than 1%
     errorRate: [
       // more than 10% of errors will abort the test
-      { threshold: 'rate < 0.05', abortOnFail: true, delayAbortEval: '1m' },
+      { threshold: 'rate < 0.08', abortOnFail: true, delayAbortEval: '1m' },
     ],
     http_req_duration: ['p(95)<2500'], // 95 percent of response times must be below 500ms
   },
